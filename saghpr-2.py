10c6673dab1b827be5ab4a0153f4903aec333f98
Sergio Enriquez Nava <eqz.nava@gmail.com>
2020-12-10 22:32:04 -0600

    Add methods for tool box and writing the methods for save data in the file

--- saghpr-2.py
+++ saghpr-2.py
@@ -25,6 +25,76 @@ class Herramienta:
         self.__Estado = esatdo
     def SetNumeroHerramientas(self, numeroHerramientas):
         self.__NumeroHerramientas = numeroHerramientas
+    def MostartHerramienta(self):
+        print('══════════════════════════════════════════════════════════════════\n')
+        print('Nombre de herramienta:',self.GetNombreHerramienta())
+        print('Medida de la herramienta:',self.GetMedida())
+        print('Uso de la herramienta:',self.GetUso())
+        print('Estado de la herramienta:',self.GetEstado())
+        print('Numero de herramientasa usar:',self.GetNumeroHerramientas(),'\n')
+        print('══════════════════════════════════════════════════════════════════\n')
+
+class CajaHerramientas:
+    def __init__(self,path):
+        self.__ListaHerramientas =[]
+        self.__Path = path
+    def CargarHerramientas(self):
+        try:
+            fichero = open(self.__Path,'r')
+        except:
+            print('\n')
+            print('╔═══════════════════════════════════════════════════════════════╗')
+            print('║   ERROR: No existe el archivo de la caja de herramientas.     ║')
+            print('╚═══════════════════════════════════════════════════════════════╝\n')
+        else:
+            cajaHerramientas = fichero.readlines()
+            fichero.close()
+            if(len(cajaHerramientas)>0):
+                for herramienta in cajaHerramientas:
+                    datos = herramienta.split('|')
+                    if(len(datos)==5):
+                        nuevaCajaHerramientas = CajaHerramientas()
+                        nuevaCajaHerramientas.SetNombreHerramienta(datos[0])
+                        nuevaCajaHerramientas.SetMedida(datos[1])
+                        nuevaCajaHerramientas.SetUso(datos[2])
+                        nuevaCajaHerramientas.SetEstado(datos[3])
+                        nuevaCajaHerramientas.SetNumeroHerramientas(datos[4])
+                        self.__ListaHerramientas = self.__ListaHerramientas + [nuevaCajaHerramientas]
+                print('INFO: Tenemos',len(self.__ListaHerramientas),'herramientas en la caja.')
+    def buildNewBoxTool(self,nuevacajaherramientas):
+        self.__ListaHerramientas = self.__ListaHerramientas + [nuevaCajaHerramientas]
+    def GuardarNuevaCaja(self):
+        try:
+            fichero = open(self.__Path,'w')
+        except:
+            print('\n')
+            print('╔═══════════════════════════════════════════════════════════════╗')
+            print('║   ERROR: No se pueden guardar los datos.                      ║')
+            print('╚═══════════════════════════════════════════════════════════════╝\n')
+        else:
+            for herramientas in self.__ListaHerramientas:
+                texto = herramienta.GetNombreHerramienta()+'|'
+                texto = texto + herramienta.GetMedida()+'|'
+                texto = texto + herramienta.GetUso()+'|'
+                texto = texto + herramienta.GetEstado()+'|'
+                texto = texto + herramienta.GetNumeroHerramientas()+'\n'
+                fichero.write(texto)
+            fichero.close()
+            print('\n')
+            print('╔═══════════════════════════════════════════════════════════════╗')
+            print('║   SUCCES: Datos almacenados exitosamente.                     ║')
+            print('╚═══════════════════════════════════════════════════════════════╝\n')
+    def MostrarCaja(self):
+        if(len(self.__ListaHerramientas)==0):
+            print('NO HAY HERRAMIENTAS EN LA CAJA')
+        elif(len(self.__ListaHerramientas)==1):
+            print('NUMERO DE HERRAMIENTAS EN LA CAJA:',len(self.__ListaHerramientas),'herramienta')
+        elif(len(self.__ListaHerramientas)>1):
+            print('NUMERO DE HERRAMIENTAS EN LA CAJA:',len(self.__ListaHerramientas),'herramientas')
+        for herramienta in self.__ListaHerramientas:
+            herramienta.MostartHerramienta()
+    #══════════════════FALTAN LOS METODOS PARA BUSCAR HERRAMIENTA Y PARA ELIMINAR HERRAMIENTAS DE LA CAJA.
+    #══════════════════FALTAN LA CLASE ALMACEN DE PARTES DE REPUESTOS Y SUS METODOS.
 
 class PartesDeRepuesto:
     def __init__(self):
@@ -56,6 +126,14 @@ class Equipo(Herramienta,PartesDeRepuesto):
         self.__Tips = tips
     def MostrarEquipo(self):
         print('\n')
-        print('')
+        print('\t\t\tE Q U I P O')
+        print('══════════════════════════════════════════════════════════════════\n')
+        print('ID de Equipo:',self.GetID())
+        print('Nombre de Equipo:',self.GetNombreEquipo())
+        print('Especificaciones tecnicas:',self.GetDataSheet())
+        print('Consejos de experiencias pasadas:',self.GetTips(),'\n')
+        print('══════════════════════════════════════════════════════════════════\n')
+        print('\tH E R R A M I E N T A S  Y  P A R T E S  D E  R E P U E S T O')
+        print('N')
 
 class MaintenanceInformationalBot()
